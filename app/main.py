f1a9d76f42817682c7f9ffc785f4abf624d1bd06
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2021-08-25 06:01:18 +0700

    Adding unit test and predict from image

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Response
-from torchvision import transforms
+from fastapi import FastAPI, Response, UploadFile, File
 from pydantic import BaseModel
+from torchvision import transforms
 from helper import api
 from model import object
+from PIL import Image
 
 app = FastAPI()
 
 
 class Input(BaseModel):
     base64str: str
-    threshold: float
 
 
 @app.get('/', status_code=200)
@@ -35,8 +35,33 @@ def get_prediction(d: Input, response: Response):
 
     pred_score = list(pred[0]['scores'].detach().numpy())
 
-    pred_t = [pred_score.index(x) for x in pred_score if x > d.threshold][-1]
+    pred_t = [pred_score.index(x) for x in pred_score if x > 0.5][-1]
     pred_boxes = pred_boxes[:pred_t + 1]
     pred_class = pred_class[:pred_t + 1]
 
     return api.builder({'boxes': pred_boxes, 'classes': pred_class}, response.status_code)
+
+
+@app.post("/predict_image", status_code=200)
+def get_prediction_image(file: UploadFile = File(...)):
+    model, COCO_INSTANCE_CATEGORY_NAMES = object.initialize()
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    img = Image.open(file.file)
+    img = transform(img)
+
+    pred = model([img])
+    pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())]
+
+    pred_boxes = [
+        [{'x1': float(i[0]), 'y1': float(i[1])}, {'x2': float(i[2]), 'y2': float(i[3])}]
+        for i in list(pred[0]['boxes'].detach().numpy())
+    ]
+
+    pred_score = list(pred[0]['scores'].detach().numpy())
+
+    pred_t = [pred_score.index(x) for x in pred_score if x > 0.5][-1]
+    pred_boxes = pred_boxes[:pred_t + 1]
+    pred_class = pred_class[:pred_t + 1]
+
+    return api.builder({'boxes': pred_boxes, 'classes': pred_class}, 200)
---
+++ app/test.py
@@ -0,0 +1,73 @@
+from fastapi.testclient import TestClient
+from main import app
+import base64
+
+client = TestClient(app)
+
+predict_response = {
+    "code": 200,
+    "message": "Success",
+    "data": {
+        "boxes": [
+            [
+                {
+                    "x1": 13.274473190307617,
+                    "y1": 12.517145156860352
+                },
+                {
+                    "x2": 712.6762084960938,
+                    "y2": 658
+                }
+            ],
+            [
+                {
+                    "x1": 15.296076774597168,
+                    "y1": 0
+                },
+                {
+                    "x2": 670.1613159179688,
+                    "y2": 648.7072143554688
+                }
+            ]
+        ],
+        "classes": [
+            "cat",
+            "dog"
+        ]
+    }
+}
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        'code': 200,
+        'message': "Success",
+        'data': "Object Detection API"
+    }
+
+
+def test_post_str_predict():
+    with open("image/dog_cat.JPG", "rb") as image_file:
+        base64str = base64.b64encode(image_file.read()).decode("utf-8")
+
+    response = client.post(
+        "/predict",
+        json={'base64str': base64str}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == predict_response
+
+
+def test_post_image_predict():
+    files = {'file': open('image/dog_cat.JPG', 'rb')}
+
+    response = client.post(
+        "/predict_image",
+        files=files
+    )
+
+    assert response.status_code == 200
+    assert response.json() == predict_response
